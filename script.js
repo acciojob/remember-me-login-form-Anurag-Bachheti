b7d2eceb37693341cc1c993597910d9f20fb3d85
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-22 16:38:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 let hiddenBtn = document.getElementById("existing");
 
- document.getElementById("submit").addEventListener("click", btnClicked);
+document.getElementById("submit").addEventListener("click", btnClicked);
 
 function btnClicked(event) {
-	event.preventDefault();
-	let name = document.getElementById("username").value;
-	let password = document.getElementById("password").value; 
-	let checkBox = document.getElementById("checkbox");
+  event.preventDefault();
 
-	if(!name && !password){
-		alert("Either Username or Password is empty")
-	}else if(name && password && checkBox.checked){
-		localStorage.setItem("userData", JSON.stringify({name, password}))
-		hiddenBtn.innerHTML = `Username: ${name}, Password: ${password}`;
-		hiddenBtn.style.display = "block";
-	}else{
-		alert(`Logged in as ${name}`)
-		localStorage.clear();
-		hiddenBtn.style.display = "none";
-	}
+  let name = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  let checkBox = document.getElementById("checkbox");
+
+  if (!name || !password) {
+    alert("Either Username or Password is empty");
+  } else if (name && password && checkBox.checked) {
+    localStorage.setItem("username", name);
+    localStorage.setItem("password", password);
+    hiddenBtn.innerHTML = "Login as existing user";
+    hiddenBtn.style.display = "block";
+  } else {
+    alert(`Logged in as ${name}`);
+    localStorage.removeItem("username");
+    localStorage.removeItem("password");
+    hiddenBtn.style.display = "none";
+  }
 }
