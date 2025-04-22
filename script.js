707ac59811d14acc7ed57f495eaa66c65d154e8f
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-04-22 15:32:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+let hiddenBtn = document.getElementById("existing");
 
+ document.getElementById("submit").addEventListener("click", btnClicked);
+
+function btnClicked(event) {
+	event.preventDefault();
+	let name = document.getElementById("username").value;
+	let password = document.getElementById("password").value; 
+	let checkBox = document.getElementById("checkbox");
+
+	if(!name && !password){
+		alert("Either Username or Password is empty")
+	}else if(name && password && checkBox.checked){
+		localStorage.setItem("userData", JSON.stringify({name, password}))
+		hiddenBtn.innerHTML = `Username: ${name}, Password: ${password}`;
+		hiddenBtn.style.display = "block";
+	}else{
+		alert(`Logged in as ${name}`)
+		localStorage.clear();
+		hiddenBtn.style.display = "none";
+	}
+}
